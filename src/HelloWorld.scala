bbde6fbf41c7b32440ccd319dc228cca3c4e7848
sharath34 <sharathchandra.muddana@sjsu.edu>
2016-09-07 23:59:18 -0700

    First Push

---
+++ src/HelloWorld.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by Sharath on 8/3/16.
+  */
+object HelloWorld {
+
+  def main(args: Array[String]) : Unit = {
+
+    println("Hello World !")
+
+  }
+}
---
+++ src/test.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by Sharath on 8/3/16.
+  */
+object test {
+
+}
